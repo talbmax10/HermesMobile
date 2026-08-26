a6540cf756b873ef00baea31dbb5593ce2569310
talbmax10 <talbmax10@users.noreply.github.com>
2026-08-26 17:09:02 +0000

    Fix build.gradle.kts syntax for Kotlin DSL

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,13 +22,16 @@ android {
 
     buildTypes {
         release {
-            minifyEnabled false
-            proguardFiles getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
     compileOptions {
-        sourceCompatibility JavaVersion.VERSION_17
-        targetCompatibility JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
         jvmTarget = "17"
