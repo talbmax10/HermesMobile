e24dbd1989e52d79a7ae1c15f353f8963e794772
talbmax10 <talbmax10@users.noreply.github.com>
2026-08-26 15:08:29 +0000

    Fix Gradle wrapper and build configuration for Android Gradle Plugin 8.4.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,14 +21,17 @@ android {
     }
 
     buildTypes {
-        release {
-            minifyEnabled false
-            proguardFiles getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
     compileOptions {
-        sourceCompatibility JavaVersion.VERSION_17
-        targetCompatibility JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
         jvmTarget = "17"
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
-    id("com.android.application") version "8.7.0" apply false
+    id("com.android.application") version "8.4.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
 }
 
-task clean(type: Delete) {
+tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
 }
